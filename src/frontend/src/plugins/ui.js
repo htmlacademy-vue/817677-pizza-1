44e42d1575aef7db0838411e55889a72aa074785
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2021-10-10 11:28:59 +0800

    animations

--- src/frontend/src/plugins/ui.js
+++ src/frontend/src/plugins/ui.js
@@ -1,4 +1,8 @@
 import Vue from "vue";
 import AppInput from "@/common/components/AppInput";
+import AppDrop from "@/common/components/AppDrop";
+import AppDrag from "@/common/components/AppDrag";
 
 Vue.component("AppInput", AppInput);
+Vue.component("AppDrop", AppDrop);
+Vue.component("AppDrag", AppDrag);
